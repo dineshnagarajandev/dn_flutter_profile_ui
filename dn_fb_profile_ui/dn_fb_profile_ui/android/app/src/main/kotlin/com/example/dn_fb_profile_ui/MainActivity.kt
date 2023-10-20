9b539c7385c31a6f2427cb7bf77803e423d685b5
Dinesh Nagarajan <dineshnagarajan19@gmail.com>
2023-10-20 13:41:11 +0530

    init

---
+++ dn_fb_profile_ui/dn_fb_profile_ui/android/app/src/main/kotlin/com/example/dn_fb_profile_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dn_fb_profile_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dn_fb_profile_ui/dn_fb_profile_ui/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Facebook Profile UI',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Facebook Profile UI'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+// EFF1F4 - bg color HEXADECIMAL code
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
